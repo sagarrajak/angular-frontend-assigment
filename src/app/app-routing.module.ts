409dc92699fb41db6da1a8a9002551183bb4e8c7
sagar rajak <sagarrajak858@gmail.com>
2019-01-17 18:46:57 -0800

    Added few changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { QuestionFiveComponent } from './question-five/question-five.component';
+import { QuestionFourComponent } from './question-four/question-four.component';
+import { QuestionOneComponent } from './question-one/question-one.component';
 import { QuestionSevenComponent } from './question-seven/question-seven.component';
-import { MyQuestionsComponent } from './my-questions/my-questions.component';
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
-import {QuestionOneComponent} from './question-one/question-one.component';
-import {QuestionTwoComponent} from './question-two/question-two.component';
-import {QuestionThreeComponent} from './question-three/question-three.component';
-import {QuestionFourComponent} from './question-four/question-four.component';
-import {QuestionFiveComponent} from './question-five/question-five.component';
-import {QuestionSixComponent} from './question-six/question-six.component';
+import { QuestionSixComponent } from './question-six/question-six.component';
+import { QuestionThreeComponent } from './question-three/question-three.component';
+import { QuestionTwoComponent } from './question-two/question-two.component';
 
 const routes: Routes = [
-  {
-    path: 'questions',
-    component: MyQuestionsComponent
-  },
   {
     path: 'question-1',
     component: QuestionOneComponent,
@@ -44,7 +39,7 @@ const routes: Routes = [
   },
   {
     path: '**',
-    redirectTo: 'questions',
+    redirectTo: 'question-1',
   },
 ];
 
--- src/app/my-questions/my-questions.component.ts
+++ src/app/my-questions/my-questions.component.ts
@@ -5,11 +5,14 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './my-questions.component.html',
   styleUrls: ['./my-questions.component.scss']
 })
-export class MyQuestionsComponent implements OnInit {
+export class MyQuestionsComponent  {
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
+  public readonly question1: string = 'question-1';
+  public readonly question2: string = 'question-2';
+  public readonly question3: string = 'question-3';
+  public readonly question4: string = 'question-4';
+  public readonly question5: string = 'question-5';
+  public readonly question6: string = 'question-6';
+  public readonly question7: string = 'question-7';
 }
